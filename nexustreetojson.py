98303949cb922cdb7ed6c6889e300cac9912fa0c
Matthew D Jones <matthew.d.jones@tessella.com>
2017-07-25 13:25:35 +0100

    use json format for attributes

--- nexustreetojson.py
+++ nexustreetojson.py
@@ -5,14 +5,26 @@ def name_to_json(tree, indent=0):
     return ' ' * indent + '"name": "' + tree.nxname + '"'
 
 
-def attrs_to_json():
-    pass
+def attrs_to_json(tree, indent=0):
+    names = sorted(tree.attrs)
+    result = ['\"attributes\": [']
+    for k in names:
+        txt1 = u' ' * indent
+        txt2 = u'"' + k
+        if nexusformat.nexus.tree.is_text(tree.attrs[k]):
+            txt3 = u'" = "' + nexusformat.nexus.tree.text(tree.attrs[k]) + '"'
+        else:
+            txt3 = u'" = ' + nexusformat.nexus.tree.text(tree.attrs[k])
+        txt = (txt1 + txt2 + txt3).replace("u'", "'")
+        result.append(txt)
+    result.append(']')
+    return '\n'.join(result)
 
 
 def tree_to_json(tree, indent=0):
     result = [indent * ' ' + '{', name_to_json(tree, indent=indent + 2)]
     if tree.attrs:
-        result.append(tree._str_attrs(indent=indent + 4))
+        result.append(attrs_to_json(tree, indent=indent + 4))
 
     if hasattr(tree, 'entries'):
         entries = tree.entries
@@ -27,6 +39,6 @@ def tree_to_json(tree, indent=0):
     return '\n'.join(result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     nexus_file = nexusformat.nexus.nxload('nexus_files/SANS2D_example.nxs')
     print(tree_to_json(nexus_file))
